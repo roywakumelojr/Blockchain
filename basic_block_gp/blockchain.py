b36d39cc383754600afb5dd86d4579196b25bf64
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2020-03-12 16:14:20 -0400

    Guess hush updated from 3 to 5

--- basic_block_gp/blockchain.py
+++ basic_block_gp/blockchain.py
@@ -114,7 +114,7 @@ class Blockchain(object):
         guess = f'{block_string}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
 
-        return guess_hash[:3] == "000"
+        return guess_hash[:5] == "00000"
 
 
 # Instantiate our Node
